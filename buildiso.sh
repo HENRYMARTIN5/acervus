686b3145a7498ad45bc4bf5d0280de4bde4ea82c
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-02-04 08:33:39 -0600

    Force symlink creation

--- buildiso.sh
+++ buildiso.sh
@@ -25,36 +25,36 @@ echo "Enabling various systemd units..."
 # dhcpcd
 echo "dhcpcd"
 mkdir -p ./airootfs/etc/systemd/system/multi-user.target.wants
-ln -s ./airootfs/etc/systemd/system/multi-user.target.wants/dhcpcd.service /usr/lib/systemd/system/dhcpcd.service
+ln -sf ./airootfs/etc/systemd/system/multi-user.target.wants/dhcpcd.service /usr/lib/systemd/system/dhcpcd.service
 # NetworkManager
 echo "NetworkManager"
 mkdir -p ./airootfs/etc/systemd/system/network-online.target.wants
-ln -s ./airootfs/etc/systemd/system/multi-user.target.wants/NetworkManager.service ./airootfs/usr/lib/systemd/system/NetworkManager.service
-ln -s ./airootfs/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service ./airootfs/usr/lib/systemd/system/NetworkManager-dispatcher.service
-ln -s ./airootfs/etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service ./airootfs/usr/lib/systemd/system/NetworkManager-wait-online.service
+ln -sf ./airootfs/etc/systemd/system/multi-user.target.wants/NetworkManager.service ./airootfs/usr/lib/systemd/system/NetworkManager.service
+ln -sf ./airootfs/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service ./airootfs/usr/lib/systemd/system/NetworkManager-dispatcher.service
+ln -sf ./airootfs/etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service ./airootfs/usr/lib/systemd/system/NetworkManager-wait-online.service
 # gdm
 echo "gdm"
-ln -s ./airootfs/etc/systemd/system/display-manager.service ./airootfs/usr/lib/systemd/system/gdm.service
+ln -sf ./airootfs/etc/systemd/system/display-manager.service ./airootfs/usr/lib/systemd/system/gdm.service
 # acpid
 echo "acpid"
-ln -s ./airootfs/etc/systemd/system/multi-user.target.wants/acpid.service ./airootfs/usr/lib/systemd/system/acpid.service
+ln -sf ./airootfs/etc/systemd/system/multi-user.target.wants/acpid.service ./airootfs/usr/lib/systemd/system/acpid.service
 # avahi-daemon
 echo "avahi-daemon"
 mkdir -p ./airootfs/etc/systemd/system/sockets.target.wants
-ln -s ./airootfs/etc/systemd/system/dbus-org.freedesktop.Avahi.service ./airootfs/usr/lib/systemd/system/avahi-daemon.service
-ln -s ./airootfs/etc/systemd/system/multi-user.target.wants/avahi-daemon.service ./airootfs/usr/lib/systemd/system/avahi-daemon.service
-ln -s ./airootfs/etc/systemd/system/sockets.target.wants/avahi-daemon.socket ./airootfs/usr/lib/systemd/system/avahi-daemon.socket
+ln -sf ./airootfs/etc/systemd/system/dbus-org.freedesktop.Avahi.service ./airootfs/usr/lib/systemd/system/avahi-daemon.service
+ln -sf ./airootfs/etc/systemd/system/multi-user.target.wants/avahi-daemon.service ./airootfs/usr/lib/systemd/system/avahi-daemon.service
+ln -sf ./airootfs/etc/systemd/system/sockets.target.wants/avahi-daemon.socket ./airootfs/usr/lib/systemd/system/avahi-daemon.socket
 # cups.service
 echo "cups.service"
 mkdir -p ./airootfs/etc/systemd/system/printer.target.wants
-ln -s ./airootfs/etc/systemd/system/printer.target.wants/cups.service ./airootfs/usr/lib/systemd/system/cups.service
-ln -s ./airootfs/etc/systemd/system/multi-user.target.wants/cups.service ./airootfs/usr/lib/systemd/system/cups.service
-ln -s ./airootfs/etc/systemd/system/sockets.target.wants/cups.socket ./airootfs/usr/lib/systemd/system/cups.socket
-ln -s ./airootfs/etc/systemd/system/multi-user.target.wants/cups.path ./airootfs/usr/lib/systemd/system/cups.path
+ln -sf ./airootfs/etc/systemd/system/printer.target.wants/cups.service ./airootfs/usr/lib/systemd/system/cups.service
+ln -sf ./airootfs/etc/systemd/system/multi-user.target.wants/cups.service ./airootfs/usr/lib/systemd/system/cups.service
+ln -sf ./airootfs/etc/systemd/system/sockets.target.wants/cups.socket ./airootfs/usr/lib/systemd/system/cups.socket
+ln -sf ./airootfs/etc/systemd/system/multi-user.target.wants/cups.path ./airootfs/usr/lib/systemd/system/cups.path
 # bluetooth
 echo "bluetooth"
-ln -s ./airootfs/etc/systemd/system/dbus-org.bluez.service ./airootfs/usr/lib/systemd/system/bluetooth.service
-ln -s ./airootfs/etc/systemd/system/bluetooth.target.wants/bluetooth.service ./airootfs/usr/lib/systemd/system/bluetooth.service
+ln -sf ./airootfs/etc/systemd/system/dbus-org.bluez.service ./airootfs/usr/lib/systemd/system/bluetooth.service
+ln -sf ./airootfs/etc/systemd/system/bluetooth.target.wants/bluetooth.service ./airootfs/usr/lib/systemd/system/bluetooth.service
 #################################
 echo "Done."
 
